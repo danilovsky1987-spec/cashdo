30dec664caf387c0d854e912674a313ddfd78030
danilovsky1987-spec <danilovsky1987@gmail.com>
2026-09-11 17:11:08 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    ВАЖНО при обновлении сайта: поменяй число в VERSION (например
    v3 → v4). Иначе телефон продолжит показывать старую версию из кэша. */
 
-const VERSION = "homedo-dengi-v5";
+const VERSION = "homedo-dengi-v9";
 const SHELL = [
   "./",
   "./index.html",
