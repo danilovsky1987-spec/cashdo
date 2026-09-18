b7eded93cb8eaef6acd8fd70f0dfd6cc62a80c3b
danilovsky1987-spec <danilovsky1987@gmail.com>
2026-09-18 13:56:30 +0300

    v45: итог недели говорит норму к сроку, а не размер недобора; цифра считается от остатка и оставшихся дней

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    ВАЖНО при обновлении сайта: поменяй число в VERSION (например
    v3 → v4). Иначе телефон продолжит показывать старую версию из кэша. */
 
-const VERSION = "homedo-dengi-v44";
+const VERSION = "homedo-dengi-v45";
 const SHELL = [
   "./",
   "./index.html",
